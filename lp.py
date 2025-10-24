8f01c57bde1ff87a4c85ab1a0f0ffc85caf3c62e
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-25 01:19:20 +0200

    Update lp.py

--- lp.py
+++ lp.py
@@ -3,7 +3,7 @@ from dhooks import Webhook, Embed, File
 
 image2_path = 'lp.png'
 
-WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN')]
+WEBHOOK_URL = [os.getenv('LP')]
 for url in WEBHOOK_URL:
     hook = Webhook(url)
 
